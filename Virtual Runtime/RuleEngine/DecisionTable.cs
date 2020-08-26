ae483a58ed9b9d0150c0cb9934db30d05f7ffb3a
wyg <1432434007@qq.com>
2020-08-26 16:20:15 +0800

    提交有关逻辑实现

--- Virtual Runtime/RuleEngine/DecisionTable.cs
+++ Virtual Runtime/RuleEngine/DecisionTable.cs
@@ -1,4 +1,5 @@
-﻿using Eap;
+﻿using System.Collections;
+using Eap;
 
 namespace ExtEap
 {
@@ -47,11 +48,83 @@ namespace ExtEap
 
         protected override Object OnExecute(AdmlObject admlobject, ExecutionStack executionstack)
         {
-            return base.OnExecute(admlobject, executionstack);
+            int nIndex;
+            string strExecutionName = executionstack.GetExecutionName();
+
+            DecisionTableMember[] decisiontablemember = new DecisionTableMember[DecisionTableMemberCount];
+
+            for (int i = 0; i < decisiontablemember.Length; i++)
+                decisiontablemember[i] = GetDecisionTableMember(i);
+
+            QuickSort(decisiontablemember, 0, decisiontablemember.Length - 1);
+
+            if (strExecutionName != null)
+            {
+                nIndex = int.Parse(strExecutionName.Substring(20));
+
+                goto DecisionTableMember;
+            }
+
+            nIndex = 0;
+
+        IsEnd:
+
+            if (nIndex >= DecisionTableMemberCount)
+                goto End;
+
+            executionstack.PushExecutionName("DecisionTableMember:" + nIndex);
+
+        DecisionTableMember:
+
+            Object objectReturn = decisiontablemember[nIndex].Execute(admlobject, executionstack);
+
+            executionstack.PopExecutionName();
+
+            if (objectReturn != null)
+            {
+                if (objectReturn is ReferableObject)
+                    executionstack.ReturnObjectReference.CreateReference((ReferableObject)objectReturn);
+
+                return objectReturn;
+            }
+
+            nIndex++;
+            goto IsEnd;
+
+            End:
+                return base.OnExecute(admlobject, executionstack);
         }
         protected override void OnParse(SyntaxErrorList syntaxerrorlist)
         {
-            base.OnParse(syntaxerrorlist);
+            for (int nIndex = 0; nIndex < DecisionTableMemberCount; nIndex++)
+                GetDecisionTableMember(nIndex).Parse(syntaxerrorlist);
+        }
+        private void QuickSort(DecisionTableMember[] decisiontablemember, int left, int right) // 快速排序，按DecisionTableMember的Priority排序，值越小优先级越高，从0开始
+        {
+            if (left < right)
+            {
+                int i = left, j = right;
+                DecisionTableMember key = decisiontablemember[left]; // 基准
+                
+                while (i < j)
+                {
+                    while (i < j && decisiontablemember[j].Priority >= key.Priority) // 从右向左找第一个小于基准key的数
+                        j--;
+
+                    if (i < j) // 在右边找到了比基准key小的数
+                        decisiontablemember[i++] = decisiontablemember[j];
+
+                    while (i < j && decisiontablemember[i].Priority < key.Priority) // 从左向右找第一个大于基准key的数
+                        i++;
+
+                    if (i < j) // 在左边找到了比基准key大的数
+                        decisiontablemember[j--] = decisiontablemember[i];
+                }
+
+                decisiontablemember[i] = key;
+                QuickSort(decisiontablemember, left, i - 1);
+                QuickSort(decisiontablemember, i + 1, right - 1);
+            }
         }
     }
 }
--- Virtual Runtime/RuleEngine/DecisionTableMember.cs
+++ Virtual Runtime/RuleEngine/DecisionTableMember.cs
@@ -25,6 +25,10 @@ namespace ExtEap
         {
             get { return m_ruleblock; }
         }
+        public int Priority
+        {
+            get { return RuleBlock.Priority; }
+        }
 
         private void OnConstruct()
         {
@@ -40,13 +44,50 @@ namespace ExtEap
 
         protected override Eap.Object OnExecute(AdmlObject admlobject, ExecutionStack executionstack)
         {
-            // TODO 待完成执行逻辑
+            string strExecutionName = executionstack.GetExecutionName();
+
+            if (strExecutionName != null)
+            {
+                if (strExecutionName == "RuleConditionValueFactor")
+                    goto RuleConditionValueFactor;
+
+                if (strExecutionName == "RuleBlock")
+                    goto RuleBlock;
+            }
+
+            executionstack.PushExecutionName("RuleConditionValueFactor");
+
+            RuleConditionValueFactor:
+
+            Object objectRuleConditionValue = RuleConditionValueFactor.Execute(admlobject, executionstack);
+            executionstack.PopExecutionName();
+            executionstack.PushObject(objectRuleConditionValue);
+
+            executionstack.PushExecutionName("RuleBlock");
+
+            RuleBlock:
+            
+            if ((bool)(((IBaseData)executionstack.GetTopObject(0)).BaseData))
+            {
+                Object objectReturn = RuleBlock.Execute(admlobject, executionstack);
+
+                executionstack.PopObject();
+                executionstack.PopExecutionName();
+
+                return objectReturn;
+            }
+
             return base.OnExecute(admlobject, executionstack);
         }
         protected override void OnParse(SyntaxErrorList syntaxerrorlist)
         {
-            // TODO 待完成校验逻辑
-            base.OnParse(syntaxerrorlist);
+            if (RuleConditionValueFactor == null)
+                syntaxerrorlist.SetSyntaxError(new StatementSyntaxError("规则条件因子不能为空", this));
+
+            if (BooleanClass.NameStatic != RuleConditionValueFactor.Parse(syntaxerrorlist))
+                syntaxerrorlist.SetSyntaxError(new StatementSyntaxError("值因子必须为" + BooleanClass.NameStatic + "类型", this));
+
+            RuleBlock.Parse(syntaxerrorlist);
         }
     }
 }
